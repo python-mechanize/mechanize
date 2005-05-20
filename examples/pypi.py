161955bc07060f1b6caf6be4fc7739814ad1d5dd
John Lee <jjl@pobox.com>
2005-05-20 21:32:11 +0000

    Update PyPI script to reflect website (only one link there now)

--- examples/pypi.py
+++ examples/pypi.py
@@ -21,27 +21,29 @@ b.select_form(nr=0)
 b["name"] = "mechanize"
 b.submit()
 
-# find latest release
-VERSION_RE = re.compile(r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<bugfix>\d+)"
-                        r"(?P<state>[ab])?(?:-pre)?(?P<pre>\d+)?$")
-def parse_version(text):
-    m = VERSION_RE.match(text)
-    if m is None:
-        raise ValueError
-    return tuple([m.groupdict()[part] for part in
-                  ("major", "minor", "bugfix", "state", "pre")])
-MECHANIZE_RE = re.compile(r"mechanize-?(.*)")
-links = b.links(text_regex=MECHANIZE_RE)
-versions = []
-for link in links:
-    m = MECHANIZE_RE.search(link.text)
-    version_string = m.group(1).strip(' \t\xa0')
-    tup = parse_version(version_string)[:3]
-    versions.append(tup)
-latest = links[versions.index(max(versions))]
+# 2005-05-20 no longer necessary, only one version there, so PyPI takes
+# us direct to PKG-INFO page
+## # find latest release
+## VERSION_RE = re.compile(r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<bugfix>\d+)"
+##                         r"(?P<state>[ab])?(?:-pre)?(?P<pre>\d+)?$")
+## def parse_version(text):
+##     m = VERSION_RE.match(text)
+##     if m is None:
+##         raise ValueError
+##     return tuple([m.groupdict()[part] for part in
+##                   ("major", "minor", "bugfix", "state", "pre")])
+## MECHANIZE_RE = re.compile(r"mechanize-?(.*)")
+## links = b.links(text_regex=MECHANIZE_RE)
+## versions = []
+## for link in links:
+##     m = MECHANIZE_RE.search(link.text)
+##     version_string = m.group(1).strip(' \t\xa0')
+##     tup = parse_version(version_string)[:3]
+##     versions.append(tup)
+## latest = links[versions.index(max(versions))]
 
 # get tarball
-b.follow_link(latest)  # to PKG-INFO page
+## b.follow_link(latest)  # to PKG-INFO page
 r = b.follow_link(text_regex=re.compile(r"\.tar\.gz"))
 filename = os.path.basename(b.geturl())
 if os.path.exists(filename):
