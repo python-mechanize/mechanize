ca62db36a14c9bcc447cb612a8fba4dd2c678629
John Lee <jjl@pobox.com>
2005-06-13 20:14:29 +0000

    Add warning about failing functional test

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,8 @@ import mechanize
 
 class ResponseTests(TestCase):
     def test_close_pickle_load(self):
+        print ("This test is expected to fail unless Python standard library"
+               "patch http://python.org/sf/1144636 has been applied")
         import pickle
 
         b = mechanize.Browser()
