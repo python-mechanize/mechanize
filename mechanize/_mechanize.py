941931c38be83a2eb3adf360e21e8c05f2228c09
John Lee <jjl@pobox.com>
2006-05-06 20:54:14 +0000

    Update version

--- mechanize/_mechanize.py
+++ mechanize/_mechanize.py
@@ -27,7 +27,7 @@ from _html import DefaultFactory
 from _Util import response_seek_wrapper, closeable_response
 import _Request
 
-__version__ = (0, 1, 0, "a", None)  # 0.1.0a
+__version__ = (0, 1, 1, "a", None)  # 0.1.1a
 
 class BrowserStateError(Exception): pass
 class LinkNotFoundError(Exception): pass
