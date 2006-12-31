6e6f49504fca225ae8b0cbad0293a7e2330e5540
John Lee <jjl@pobox.com>
2006-12-31 16:54:58 +0000

    Note non working-ness of pypi.py example script (it doesn't work because the PyPI search feature seems to have gone away)

--- examples/pypi.py
+++ examples/pypi.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# ------------------------------------------------------------------------
+# THIS SCRIPT IS CURRENTLY NOT WORKING, SINCE PYPI's SEARCH FEATURE HAS
+# BEEN REMOVED!
+# ------------------------------------------------------------------------
+
 # Search PyPI, the Python Package Index, and retrieve latest mechanize
 # tarball.
 
