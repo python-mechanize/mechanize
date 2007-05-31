07a7cbc1c779d87a2b2384269f8943005e4a1228
John Lee <jjl@pobox.com>
2007-05-31 00:04:54 +0000

    Update version

--- mechanize/_mechanize.py
+++ mechanize/_mechanize.py
@@ -17,7 +17,7 @@ import _response
 import _request
 import _rfc3986
 
-__version__ = (0, 1, 7, "b", None)  # 0.1.7b
+__version__ = (0, 1, 8, "b", None)  # 0.1.8b
 
 class BrowserStateError(Exception): pass
 class LinkNotFoundError(Exception): pass
