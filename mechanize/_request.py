6173d46dd6a14cff28a8b483b6aa999f92bbd6a8
John Lee <jjl@pobox.com>
2009-12-21 23:52:44 +0000

    Remove code now present in _urllib2_fork

--- mechanize/_request.py
+++ mechanize/_request.py
@@ -10,7 +10,6 @@ COPYING.txt included with the distribution).
 
 import urllib, logging
 
-from _clientcookie import request_host_lc
 import _rfc3986
 import _sockettimeout
 import _urllib2_fork
@@ -34,55 +33,11 @@ class Request(_urllib2_fork.Request):
                  "(contains illegal characters) %r" % url)
         _urllib2_fork.Request.__init__(self, url, data, headers)
         self.selector = None
-        self.unredirected_hdrs = {}
         self.visit = visit
         self.timeout = timeout
-
-        # All the terminology below comes from RFC 2965.
-        self.unverifiable = unverifiable
-        # Set request-host of origin transaction.
-        # The origin request-host is needed in order to decide whether
-        # unverifiable sub-requests (automatic redirects, images embedded
-        # in HTML, etc.) are to third-party hosts.  If they are, the
-        # resulting transactions might need to be conducted with cookies
-        # turned off.
-        if origin_req_host is None:
-            origin_req_host = request_host_lc(self)
-        self.origin_req_host = origin_req_host
 
     def get_selector(self):
         return urllib.splittag(self.__r_host)[0]
 
-    def get_origin_req_host(self):
-        return self.origin_req_host
-
-    def is_unverifiable(self):
-        return self.unverifiable
-
-    def add_unredirected_header(self, key, val):
-        """Add a header that will not be added to a redirected request."""
-        self.unredirected_hdrs[key.capitalize()] = val
-
-    def has_header(self, header_name):
-        """True iff request has named header (regular or unredirected)."""
-        return (header_name in self.headers or
-                header_name in self.unredirected_hdrs)
-
-    def get_header(self, header_name, default=None):
-        return self.headers.get(
-            header_name,
-            self.unredirected_hdrs.get(header_name, default))
-
-    def header_items(self):
-        hdrs = self.unredirected_hdrs.copy()
-        hdrs.update(self.headers)
-        return hdrs.items()
-
     def __str__(self):
         return "<Request for %s>" % self.get_full_url()
-
-    def get_method(self):
-        if self.has_data():
-            return "POST"
-        else:
-            return "GET"
