baf81686e7d7d681b3e5354f0ddaafc83b91a20c
John Lee <jjl@pobox.com>
2010-04-18 14:09:27 +0100

    Fix a Python 2.4 incompatibility

--- examples/pypi.py
+++ examples/pypi.py
@@ -23,7 +23,7 @@ def download_mechanize():
     browser.submit()
     browser.follow_link(text_regex="mechanize-?(.*)")
     link = browser.find_link(text_regex=r"\.tar\.gz")
-    filename = os.path.basename(urlparse.urlsplit(link.url).path)
+    filename = os.path.basename(urlparse.urlsplit(link.url)[2])
     if os.path.exists(filename):
         sys.exit("%s already exists, not grabbing" % filename)
     browser.retrieve(link.url, filename)
