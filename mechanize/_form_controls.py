0d5a0746fd146b480210589bc9e8ca29e369a415
Kovid Goyal <kovid@kovidgoyal.net>
2017-03-13 09:13:10 +0530

    Drop support for the <isindex> tag

--- mechanize/_form_controls.py
+++ mechanize/_form_controls.py
@@ -585,81 +585,6 @@ class FileControl(ScalarControl):
 
 
 # ---------------------------------------------------
-class IsindexControl(ScalarControl):
-    """ISINDEX control.
-
-    ISINDEX is the odd-one-out of HTML form controls.  In fact, it isn't really
-    part of regular HTML forms at all, and predates it.  You're only allowed
-    one ISINDEX per HTML document.  ISINDEX and regular form submission are
-    mutually exclusive -- either submit a form, or the ISINDEX.
-
-    Having said this, since ISINDEX controls may appear in forms (which is
-    probably bad HTML), ParseFile / ParseResponse will include them in the
-    HTMLForm instances it returns.  You can set the ISINDEX's value, as with
-    any other control (but note that ISINDEX controls have no name, so you'll
-    need to use the type argument of set_value!).  When you submit the form,
-    the ISINDEX will not be successful (ie., no data will get returned to the
-    server as a result of its presence), unless you click on the ISINDEX
-    control, in which case the ISINDEX gets submitted instead of the form:
-
-    form.set_value("my isindex value", type="isindex")
-    mechanize.urlopen(form.click(type="isindex"))
-
-    ISINDEX elements outside of FORMs are ignored.  If you want to submit one
-    by hand, do it like so:
-
-    url = urlparse.urljoin(page_uri, "?"+urllib.quote_plus("my isindex value"))
-    result = mechanize.urlopen(url)
-
-    """
-
-    def __init__(self, type, name, attrs, index=None):
-        ScalarControl.__init__(self, type, name, attrs, index)
-        if self._value is None:
-            self._value = ""
-
-    def is_of_kind(self, kind):
-        return kind in ["text", "clickable"]
-
-    def _totally_ordered_pairs(self):
-        return []
-
-    def _click(self, form, coord, return_type, request_class=_request.Request):
-        # Relative URL for ISINDEX submission: instead of "foo=bar+baz",
-        # want "bar+baz".
-        # This doesn't seem to be specified in HTML 4.01 spec. (ISINDEX is
-        # deprecated in 4.01, but it should still say how to submit it).
-        # Submission of ISINDEX is explained in the HTML 3.2 spec, though.
-        parts = self._urlparse(form.action)
-        rest = parts[:-2]
-        parts = rest + (urllib.quote_plus(self.value), None)
-        url = self._urlunparse(parts)
-        req_data = url, None, []
-
-        if return_type == "pairs":
-            return []
-        elif return_type == "request_data":
-            return req_data
-        else:
-            return request_class(url)
-
-    def __str__(self):
-        value = self.value
-        if value is None:
-            value = "<None>"
-
-        infos = []
-        if self.disabled:
-            infos.append("disabled")
-        if self.readonly:
-            infos.append("readonly")
-        info = ", ".join(infos)
-        if info:
-            info = " (%s)" % info
-
-        return "<%s(%s)%s>" % (self.__class__.__name__, value, info)
-
-
 # ---------------------------------------------------
 class IgnoreControl(ScalarControl):
     """Control that we're not interested in.
@@ -1860,7 +1785,6 @@ class HTMLForm:
         "password": PasswordControl,
         "hidden": HiddenControl,
         "textarea": TextareaControl,
-        "isindex": IsindexControl,
         "file": FileControl,
         "button": IgnoreControl,
         "buttonbutton": IgnoreControl,
@@ -2383,7 +2307,7 @@ class HTMLForm:
         addition to the types possible for <input> HTML tags: "text",
         "password", "hidden", "submit", "image", "button", "radio", "checkbox",
         "file" we also have "reset", "buttonbutton", "submitbutton",
-        "resetbutton", "textarea", "select" and "isindex").
+        "resetbutton", "textarea", "select").
 
         If kind is specified, then the control must fall into the specified
         group, each of which satisfies a particular interface.  The types are
