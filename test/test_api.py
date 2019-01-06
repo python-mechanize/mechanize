5e835d45e28432570d60574a6e1615d0e42c4173
Kovid Goyal <kovid@kovidgoyal.net>
2019-01-06 21:59:52 +0530

    Fix test_api for py 3

--- test/test_api.py
+++ test/test_api.py
@@ -6,7 +6,10 @@ class ImportTests(unittest.TestCase):
     def test_import_all(self):
         # the following will raise an exception if __all__ contains undefined
         # classes
-        from mechanize import *
+        import mechanize
+        from mechanize import __all__
+        for x in __all__:
+            getattr(mechanize, x)
 
 
 if __name__ == "__main__":
