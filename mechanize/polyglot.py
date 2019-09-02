cd37011a8a61595c003d3c0c1fa60010bc576a8c
Kovid Goyal <kovid@kovidgoyal.net>
2019-09-02 15:05:13 +0530

    Fix incorrect license info in a couple of source files
    Fixes #48

--- mechanize/polyglot.py
+++ mechanize/polyglot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf-8
-# License: GPLv3 Copyright: 2018, Kovid Goyal <kovid at kovidgoyal.net>
+# Copyright: 2018, Kovid Goyal <kovid at kovidgoyal.net>
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
--- test/test_http.py
+++ test/test_http.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf-8
-# License: GPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
+# Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
 
 from unittest import TestCase
