a3fcd0a31d0d6a80ed87aa1ac760a819da6901fc
Kovid Goyal <kovid@kovidgoyal.net>
2019-09-02 15:07:19 +0530

    Remove license info from a few more source files, COPYRIGHT is single source of truth

--- publish.py
+++ publish.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf-8
-# License: BSD Copyright: 2017, Kovid Goyal <kovid at kovidgoyal.net>
+# Copyright: 2017, Kovid Goyal <kovid at kovidgoyal.net>
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
